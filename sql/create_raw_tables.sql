522ae8cc17f422571dab82864e5658f892b8d121
t.iskakov <lightx_design@yahoo.com>
2026-08-05 20:17:49 +0600

    add sql

---
+++ sql/create_raw_tables.sql
@@ -0,0 +1,43 @@
+CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE TABLE IF NOT EXISTS raw.customers (
+    customer_id BIGINT PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    country TEXT,
+    city TEXT,
+    registered_at TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS raw.products (
+    product_id BIGINT PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price NUMERIC(12, 2) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS raw.orders (
+    order_id BIGINT PRIMARY KEY,
+    customer_id BIGINT NOT NULL,
+    order_status TEXT NOT NULL,
+    order_created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS raw.order_items (
+    order_item_id BIGINT PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    unit_price NUMERIC(12, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS raw.payments (
+    payment_id BIGINT PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    payment_method TEXT NOT NULL,
+    payment_status TEXT NOT NULL,
+    amount NUMERIC(12, 2) NOT NULL,
+    paid_at TIMESTAMP
+);
